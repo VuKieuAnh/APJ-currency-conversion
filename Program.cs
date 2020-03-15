1f304a01e1bb36a7c9655827447ab3f8edf7ee64
VuKieuAnh <vukieuanh.hnue@gmail.com>
2020-03-15 22:28:40 +0700

    init

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ASP_currency_conversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float rate, vnd, usd;
+            Console.WriteLine("Enter the exchange rate");
+            rate = float.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the amount of USD");
+            usd = float.Parse(Console.ReadLine());
+            vnd = rate*usd;
+            Console.WriteLine("Amount VND respectively " + vnd);
+        }
+    }
+}
